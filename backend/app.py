63fb18bf901fe6730e9368819344349b22e3f336
zhang.yizhe@northeastern.edu <zhang.yizhe@northeastern.edu>
2021-03-03 19:04:32 -0500

    add python process

--- backend/app.py
+++ backend/app.py
@@ -83,41 +83,59 @@ def index():
 # 路由传参
 class jsonAPI(Resource):
     # http://127.0.0.1:5000/api/值
+    # terminal sql
     def get(self,key):
-        # try:
-        rst = query_db('select * from test')
-        # 取第一行
-        rst2 = [rst[0]["id"],rst[0]["testvalue"]]
-        jsonObj = {"result":rst2,'function':1}
-        return jsonify(jsonObj)
-        # except Exception:
-            # return jsonify({"error":"error"})
-    
-    # http://127.0.0.1:5000/api/值
-    # 传{"key":"值"}
-    def post(self,key):
         try:
-            args = parser.parse_args()
-            key = eval(args['key'])
-            jsonObj = {"result":key,'function':1}
+            rst = query_db('select * from test')
+            # 取第一行
+            rst2 = [rst[0]["id"],rst[0]["testvalue"]]
+            jsonObj = {"result":rst2,'function':1}
             return jsonify(jsonObj)
         except Exception:
             return jsonify({"error":"error"})
+    
+    # http://127.0.0.1:5000/api/值
+    # 传{"key":"值"}
+    # def post(self,key):
+    #     try:
+    #         args = parser.parse_args()
+    #         key = eval(args['key'])
+    #         jsonObj = {"result":key,'function':1}
+    #         return jsonify(jsonObj)
+    #     except Exception:
+    #         return jsonify({"error":"error"})
 api.add_resource(jsonAPI, '/api/<key>')
 
 # 问号传参
 class jsonAPI2(Resource):
     # http://127.0.0.1:5000/api/?pkg=值
+    # terminal python 单行
+    # terminal python file (接收前端字符串"class Run:\n  def run():\n    ")
     def get(self):
         try:
             pkg = request.args.get("pkg","")
+            # 单行代码
             jsonObj = {"result":eval(pkg),'function':2}
+
+            # # 多行代码
+            # # 写入文件
+            # f = open(f'Coding/online_codes.py', 'w+')
+            # f.write(key)
+            # f.close()
+            # # 重新导入文件中函数
+            # import Coding.online_codes as oc
+            # reload(oc)
+            # # 返回运行结果
+            # temp = oc.Run.run()
+            # jsonObj = {"result":temp,'function':2}
+
             return jsonify(jsonObj)
         except Exception:
             return jsonify({"error":"error"})
     
     # http://127.0.0.1:5000/api/
     # 传{"key":"值"}
+    # terminal linux
     def post(self):
         try:
             args = parser.parse_args()
